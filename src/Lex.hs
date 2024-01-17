321f32117474fe6f3e1f9105331f8133652d31ab
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-01-17 11:47:18 -0600

    Implement lexing

--- src/Lex.hs
+++ src/Lex.hs
@@ -1,66 +1,20 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Lex where
 
+import Control.Monad
 import Text.Parsec
 import Text.Parsec.Char
 import Text.ParserCombinators.Parsec.Combinator
 
-{-
+{- Helpers -}
 
-Lexer Grammar
-(from https://standards.scheme.org/official/r3rs.pdf)
+-- in the newer version of Text.Parsec.Char
+string' :: Stream s m Char => String -> ParsecT s u m String
+string' s = try (string s)
 
-token -> identifier | boolean | number
-       | character | string | ( | ) | #( | ' | .
-
-delimeter -> whitespace | ( | ) | " | ;
-
-whitespace -> space | newline
-
-comment -> ; <ignore all subsequent chars until newline>
-
-atmosphere -> whitespace | comment
-
-intertoken_space -> atmosphere*
-
-identifier -> initial subsequent* | pecuilar_identifier
-
-initial -> letter | special_initial
-
-letter -> a | b | ... | z
-
-special_initial -> ! | $ | % | & | * | / | : | < | = 
-                 | > | ? | ~ | _ | ^
-
-subsequent -> initial | digit | special_subsequent
-
-digit -> 0 | 1 | ... | 9
-
-special_subsequent -> . | + | -
-
-pecuilar_identifier -> + | -
-
-syntactic_keyword -> expression_keyword | else | define
-
-expression_keyword -> quote | lambda | if | set!
-                    | begin | cond | and | or | case
-                    | let | let* | letrec | do
-
-variable -> <identifier, which isn't also a syntactic_keyword>
-
-boolean -> #t | #f
-
-character -> #\ <char> | #\ character_name
-
-character_name -> space | newline
-
-string -> " string_element* "
-
-string_element -> <non-"-char> | \" | \\
-
-number -> [sign] digit+ [. digit*]
-        | [sign] . digit+
-
--}
+{- Lexer Grammar -}
+{- (from https://standards.scheme.org/official/r3rs.pdf, pp. 30-31) -}
 
 {- Data Structures/ Types -}
 
@@ -74,6 +28,7 @@ data Token = TokId String
            | TokHashOpen -- #(
            | TokQuote
            | TokPeriod
+      deriving (Show)
 
 data ArithSign = Positive | Negative
 
@@ -81,20 +36,37 @@ type LexFn = Parsec String ()
 
 {- Lex Functions -}
 
+tokenize :: String -> Either ParseError [Token]
+tokenize = parse lexTokenStream ""
 
--- token -> identifier | boolean | number
---        | character | string | ( | ) | #( | ' | .
--- 
--- delimeter -> whitespace | ( | ) | " | ;
--- 
--- whitespace -> space | newline
+-- token_stream -> (token intertoken_space)* eof
+--
+-- intertoken_space -> atmosphere*
 -- 
 -- comment -> ; <ignore all subsequent chars until newline>
 -- 
--- atmosphere -> whitespace | comment
--- 
--- intertoken_space -> atmosphere*
--- 
+-- atmosphere -> space | newline | comment
+
+lexTokenStream :: LexFn [Token]
+lexTokenStream = many (lexToken <* lexIntertokenSpace)
+    where lexIntertokenSpace = many (void space <|> lexComment)
+          lexComment = void (char ';' *> many (noneOf "\n") <* newline)
+
+-- token -> identifier | boolean | number
+--        | character | string | ( | ) | #( | ' | .
+
+lexToken :: LexFn Token
+lexToken = TokId <$> try lexIdentifier
+       <|> TokBool <$> try lexBoolean
+       <|> TokNum <$> try lexNumber
+       <|> TokChar <$> try lexCharacter
+       <|> TokString <$> try lexString
+       <|> TokOpen <$ char '('
+       <|> TokClose <$ char ')'
+       <|> TokHashOpen <$ string "#("
+       <|> TokQuote <$ char '\''
+       <|> TokPeriod <$ char '.'
+
 -- identifier -> initial subsequent* | pecuilar_identifier
 -- 
 -- initial -> letter | special_initial
@@ -111,42 +83,48 @@ type LexFn = Parsec String ()
 -- special_subsequent -> . | + | -
 -- 
 -- pecuilar_identifier -> + | -
--- 
--- syntactic_keyword -> expression_keyword | else | define
--- 
--- expression_keyword -> quote | lambda | if | set!
---                     | begin | cond | and | or | case
---                     | let | let* | letrec | do
--- 
--- variable -> <identifier, which isn't also a syntactic_keyword>
--- 
--- boolean -> #t | #f
--- 
--- character -> #\ <char> | #\ character_name
--- 
--- character_name -> space | newline
--- 
--- string -> " string_element* "
--- 
--- string_element -> <non-"-char> | \" | \\
---
 
+lexIdentifier :: LexFn String
+lexIdentifier = (:) <$> lexInitial <*> many lexSubsequent
+            <|> (string "+" <|> string "-")
+      where lexInitial = letter <|> oneOf "!$%&*/:<=>?~_^"
+            lexSubsequent = lexInitial <|> digit <|> oneOf ".+-"
+
+-- boolean -> #t | #f
+
+lexBoolean :: LexFn Bool
+lexBoolean = True <$ string' "#t"
+         <|> False <$ string' "#f"
+
+-- character -> #\ <char> | #\ character_name
+--
+-- character_name -> space | newline
+
+lexCharacter :: LexFn Char
+lexCharacter = string "#\\" *> (lexCharName <|> anyChar)
+      where lexCharName = ' ' <$ string' "space"
+                      <|> '\n' <$ string' "newline"
+
+
+-- string -> " string_element* "
+--
+-- string_element -> <non-"-or-\-char> | \" | \\
+
+lexString :: LexFn String
+lexString = between (char '"') (char '"') (many lexStrElem)
+      where lexStrElem  = char '\\' *> (char '\\' <|> char '"')
+                      <|> noneOf "\"\\"
 
 -- number -> [sign] digit+ [. digit*]
 --         | [sign] . digit+
-
-lexNumber :: LexFn Double
-lexNumber = do sign <- lexSign
-               leftDigits <- many digit
-               let lexDecDigits = if length leftDigits == 0 then many1 digit else many digit
-               decimalPart <- (string "." <> lexDecDigits) <|> return ""
-               return . strToD' $ leftDigits ++ decimalPart
-    where strToD' ('+':s) = read s
-          strToD' s = read s
-
+--
 -- sign -> + | - | <nothing>
 
-lexSign :: LexFn Char
-lexSign = char '+'
-      <|> char '-'
-      <|> return '+' 
+lexNumber :: LexFn Double
+lexNumber = strToD' <$> lexSign <> (many1 digit <> option "" (string "." <> many digit) <|>
+                                    string "." <> many1 digit)
+    where lexSign = string "+" <|> string "-" <|> return "+"
+          strToD' ('+':s) = strToD' s
+          strToD' ('-':s) = -1 * strToD' s
+          strToD' ('.':s) = read ("0." ++ s)
+          strToD' s = read s
