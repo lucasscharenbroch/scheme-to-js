c7fcaccf172b1b57b9713fe712c5690edfa946c1
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-01-20 13:58:58 -0600

    Add cli option handling

--- src/Main.hs
+++ src/Main.hs
@@ -4,77 +4,70 @@ import Parse
 import Generate
 import Text.Parsec (ParseError)
 import Data.List
+import System.IO
+import System.Console.GetOpt
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
 
--- TODO remove
-resToStr :: Either ParseError (Either ParseError [Program]) -> String
-resToStr (Left err) = "tokenizing error: " ++ show err
-resToStr (Right (Left err)) = "parse error: " ++ show err
-resToStr (Right (Right p)) = intercalate "\n" $ map gen p
+{- CLI Arg Parse -}
 
-cases = [
-        -- "(())()#(abc abcdef + - + 432.432"
-        -- ""
-        -- "xyz&&~"
-        -- "xyz&&~["
-        -- "ab12"
-        -- "\"abcd\"123\"efgh\"1.2"
-        -- "'abc+123 +123 -123 +.3"
-        -- "test words and symbols 1 and ot7er construction (s 9 9) ; comment\nnot a comment"
-        -- "test words (and) symbols 1 (and) ot7er construction (s 9 9) ; comment\nnot a comment"
-        -- "\\#hi"
-        -- "#t #f"
-        -- "12ab"
-        -- "#true"
-        -- "#\\abc"
-        -- "#\\a bc"
-        -- "(define a b)"
-        -- "(define a 3)"
-        -- "(((((((((((((((define a 3"
-        -- "(((and a b) 1 2"
-        -- "("
-        -- "(and)"
-        -- "(andd)"
-        -- "(+ 1 2 (* 3 4))"
-        -- "(1 2 ⊆)"
-        -- "(((and a b) 1 2)"
-        -- "(((and a b) 1 2))"
-        -- "()"
-        -- "(lambda x 1)"
-        -- "(lambda (x y) 1)"
-        -- "(lambda (x y) (+ 1 2))"
-        -- "(lambda () y)"
-        -- "(lambda (x) (define y 2) y)"
-        -- "(lambda (x) (define y 2) (define z 3) y)"
-        -- "(define (z) 4)"
-        -- "(define (z and) 4)"
-        -- "(define (z q w) (+ z w))"
-        -- "(define (z . q w) (+ z w))"
-        -- "(define (z q . w) (define a b) (define (c d) e) (+ z w))"
-        -- "(define (z q . w) (+ z w))"
-        -- "(define (z . w) (+ z w))"
-        -- "(lambda (x) (define y 2) (define (z) 3) y)"
-        -- "'(a b c)"
-        -- "'()"
-        -- "'xyz"
-        -- "'123.345"
-        -- "'#(vec of things)"
-        -- "(list 1 2 3)"
-        -- "'(a b c (d e f (1 2 3 ())))"
-        -- "(if #t (if #f (+ 1 2) 9) #t)"
-        -- "(lambda (x y z) (+ x y z))"
-        -- "(define x 2)"
-        -- "(define (x) 2)"
-        -- "(cond ())"
-        -- "(cond)"
-        -- "(cond (#t (if #t 123 999)) (#f 456) (xyz) (else 789))"
-        -- "(let ((a 1) (b 2) (c 3)) (define d 123) (+ a b c d))"
-        -- "(define Abc+- 123)"
-        -- "+"
-        -- "xyz!"
-        -- "1bc"
-        -- "'(a b c 1 2 3 #(1 2 3))"
-        "(quote (a b c 1 2 3 #(1 2 3)))"
+usageHeader = "Usage: s2j input-files [options]"
+
+data CliOption = HelpFlag
+               | VerboseFlag
+               | OutputFlag String
+               | InputFlag String
+
+optDescriptions :: [OptDescr CliOption]
+optDescriptions = [
+        Option "h" ["help"] (NoArg HelpFlag) "print help information",
+        Option "v" ["verbose"] (NoArg VerboseFlag) "enable verbosity",
+        Option "o" ["output-file"] (ReqArg OutputFlag "filename") "set output file name"
     ]
 
+handleArgv :: IO [CliOption]
+handleArgv = do argv <- getArgs
+                let (opts, nonOptions, errs) = getOpt (ReturnInOrder InputFlag) optDescriptions argv
+                if not (null errs) then hPutStrLn stderr ("encountered error: " ++ head errs) >> exitFailure
+                else if not (null nonOptions) then hPutStrLn stderr ("invalid option: " ++ head nonOptions) >> exitFailure
+                else return opts
+
+sortOptions :: [CliOption] -> (Bool, Bool, [String], [String])
+sortOptions = foldl foldOpts (False, False, [], [])
+    where foldOpts (h, v, i, o) HelpFlag = (True, v, i, o)
+          foldOpts (h, v, i, o) VerboseFlag = (h, True, i, o)
+          foldOpts (h, v, i, o) (InputFlag inFile) = (h, v, i ++ [inFile], o)
+          foldOpts (h, v, i, o) (OutputFlag outFile) = (h, v, i, o ++ [outFile])
+
+transpile :: String -> Either String String
+transpile input = case tokenize input of
+    Left err -> Left $ "Lexing Error: " ++ show err
+    Right toks -> case programize toks of
+        Left err -> Left $ "Parsing Error: " ++ show err
+        Right programs -> Right . intercalate "\n" . map gen $ programs
+
+validateInfiles :: [String] -> IO ()
+validateInfiles [] = hPutStrLn stderr "expected input file (pass \"-\" to read from stdin)" >> exitFailure
+validateInfiles _ = return ()
+
+validateOutfile :: [String] -> IO String
+validateOutfile [] = return "out.scm.js"
+validateOutfile [filename] = return filename
+validateOutfile _ = hPutStrLn stderr "Too many output files specified (expected 0 or 1)" >> exitFailure
+
+main :: IO ()
 main = do
-    mapM (putStrLn . resToStr . fmap programize . tokenize) cases
+    (isHelp, isVerbose, inputs, outputs) <- sortOptions <$> handleArgv
+    if isHelp then putStr $ usageInfo usageHeader optDescriptions
+    else main' isVerbose inputs outputs
+
+main' :: Bool -> [String] -> [String] -> IO ()
+main' isVerbose inputs outputs = do
+    validateInfiles inputs
+    outputFile <- validateOutfile outputs
+    inputText <- concat <$> mapM _readFile inputs
+    case transpile inputText of
+        Left err -> hPutStrLn stderr err >> exitFailure
+        Right outputText -> writeFile outputFile outputText
+    where _readFile "-" = getContents
+          _readFile name = readFile name
